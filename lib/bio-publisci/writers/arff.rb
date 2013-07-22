16e5e0a46d2e989621bd683ac8f7a16f59991951
wstrinz <wstrinz@gmail.com>
2013-07-21 19:11:33 -0500

    update arff writer for new queries

--- lib/bio-publisci/writers/arff.rb
+++ lib/bio-publisci/writers/arff.rb
@@ -1,12 +1,12 @@
 module R2RDF
-	module Writer
-		class ARFF
-			include R2RDF::Query
-			include R2RDF::Parser
-			include R2RDF::Analyzer
+  module Writer
+    class ARFF
+      include R2RDF::Query
+      include R2RDF::Parser
+      include R2RDF::Analyzer
 
-			def build_arff(relation, attributes, data, source)
-				str = <<-EOS
+      def build_arff(relation, attributes, data, source)
+        str = <<-EOS
 % 1. Title: #{relation.capitalize} Database
 %
 % 2. Sources:
@@ -16,51 +16,51 @@ module R2RDF
 
 EOS
 
-				Hash[attributes.sort].map{|attribute,type|
-					str << "@ATTRIBUTE #{attribute} #{type}\n"
-				}
+        Hash[attributes.sort].map{|attribute,type|
+          str << "@ATTRIBUTE #{attribute} #{type}\n"
+        }
 
-				str << "\n@DATA\n"
-				data.map { |d| str << Hash[d[1].sort].values.join(',') + "\n" }
+        str << "\n@DATA\n"
+        data.map { |d| str << Hash[d[1].sort].values.join(',') + "\n" }
 
-				str
-			end
+        str
+      end
 
-			def from_turtle(turtle_file, verbose=false)
-				puts "loading #{turtle_file}" if verbose
-				repo = RDF::Repository.load(turtle_file)
-				puts "loaded #{repo.size} statements into temporary repo" if verbose
-				
-				dims = execute_from_file("dimensions.rq",repo,:graph).to_h.map{|d| d[:label].to_s}
-				meas = execute_from_file("measures.rq",repo,:graph).to_h.map{|m| m[:labels].to_s}
-				relation = execute_from_file("dataset.rq",repo,:graph).to_h.first[:label].to_s
-				codes = execute_from_file("codes.rq",repo,:graph).to_h.map{|e| e.values.map(&:to_s)}.inject({}){|h,el|
-					(h[el.first]||=[]) << el.last; h
-				}
+      def from_turtle(turtle_file, verbose=false)
+        puts "loading #{turtle_file}" if verbose
+        repo = RDF::Repository.load(turtle_file)
+        puts "loaded #{repo.size} statements into temporary repo" if verbose
+        
+        dims = execute_from_file("dimensions.rq",repo,:graph).to_h.map{|d| [d[:dimension].to_s, d[:label].to_s]}
+        meas = execute_from_file("measures.rq",repo,:graph).to_h.map{|m| [m[:measure].to_s, m[:label].to_s]}
+        relation = execute_from_file("dataset.rq",repo,:graph).to_h.first[:label].to_s
+        codes = execute_from_file("codes.rq",repo,:graph).to_h.map{|e| e.values.map(&:to_s)}.inject({}){|h,el|
+          (h[el.first]||=[]) << el.last; h
+        }
 
-				data = observation_hash(execute_from_file("observations.rq",repo,:graph), true)
-				attributes = {}
-				(dims | meas).map{|component|
-					attributes[component] = case recommend_range(data.map{|o| o[1][component]})
-						when "xsd:int"
-							"integer"
-						when "xsd:double"
-							"real"
-						when :coded
-							if dims.include? component
-								"{#{codes[component].join(',')}}"
-							else
-								"string"
-							end
-						end
-				}
+        data = observation_hash(execute_from_file("observations.rq",repo,:graph), true)
+        attributes = {}
+        (dims | meas).map{|component|
+          attributes[component[1]] = case recommend_range(data.map{|o| o[1][component[1]]})
+            when "xsd:int"
+              "integer"
+            when "xsd:double"
+              "real"
+            when :coded
+              if dims.include? component
+                "{#{codes[component[1]].join(',')}}"
+              else
+                "string"
+              end
+            end
+        }
+        puts attributes
+        build_arff(relation, attributes, data, turtle_file)
+      end
 
-				build_arff(relation, attributes, data, turtle_file)
-			end
-
-			def from_store(endpoint_url,variable_in=nil, variable_out=nil, verbose=false)
-				raise "not implemented yet"
-			end
-		end
-	end
+      def from_store(endpoint_url,variable_in=nil, variable_out=nil, verbose=false)
+        raise "not implemented yet"
+      end
+    end
+  end
 end
