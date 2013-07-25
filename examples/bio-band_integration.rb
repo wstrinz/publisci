62d3c176380a7aa7773b106af8c0909788af8340
wstrinz <wstrinz@gmail.com>
2013-07-25 15:32:24 -0500

    move bio-band integration into example script since cucumber doesn't like it (may be partly my fault)

---
+++ examples/bio-band_integration.rb
@@ -0,0 +1,9 @@
+require 'bio-band'
+require 'bio-publisci'
+
+f = open(File.dirname(__FILE__) + '/../resources/weather.numeric.arff')
+clustering = Weka::Clusterer::SimpleKMeans::Base
+clustering.set_options "-N 5"
+clustering.set_data(Core::Parser::parse_ARFF(f.path))
+clustered = clustering.new
+puts clustered
