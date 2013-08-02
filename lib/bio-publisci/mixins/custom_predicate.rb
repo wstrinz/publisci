e50e7aac8cdc0e79f5edadf9f8a1cac845664713
wstrinz <wstrinz@gmail.com>
2013-08-01 22:11:54 -0500

    add dereferencing of objects for custom predicates

--- lib/bio-publisci/mixins/custom_predicate.rb
+++ lib/bio-publisci/mixins/custom_predicate.rb
@@ -1,10 +1,14 @@
 module PubliSci
   module CustomPredicate
     def has(predicate, object)
-      predicate = RDF::Resource(predicate) if RDF::Resource(predicate).valid?
-      obj = RDF::Resource(object)
-      obj = RDF::Literal(object) unless obj.valid?
-      ((@custom ||= {})[predicate] ||= []) << obj
+      if object.is_a? Symbol
+        obj = object
+      else
+        predicate = RDF::Resource(predicate) if RDF::Resource(predicate).valid?
+        obj = RDF::Resource(object)
+        obj = RDF::Literal(object) unless obj.valid?
+      end
+        ((@custom ||= {})[predicate] ||= []) << obj
     end
     alias_method :set, :has
 
@@ -17,6 +21,14 @@ module PubliSci
         custom.map{|k,v|
           pk = k.respond_to?(:to_base) ? k.to_base : k
           v.map{|vv|
+            if vv.is_a? Symbol
+
+              deref = Prov.registry.values.map{|h|
+                h[vv] if vv
+              }.reject{|x| x==nil}
+              raise "Unknown Element #{vv}" unless deref.size > 0
+              vv = RDF::Resource(deref.first)
+            end
             str << "\t#{pk} #{vv.to_base} ;\n"
           }
         }
