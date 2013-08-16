48c127d24b75981d55186f2fcdf03d7c117e99bc
Will Strinz <wstrinz@gmail.com>
2013-08-16 17:24:50 -0500

    add from store to arff writer

--- lib/bio-publisci/writers/arff.rb
+++ lib/bio-publisci/writers/arff.rb
@@ -57,9 +57,33 @@ EOS
         build_arff(relation, attributes, data, turtle_file)
       end
 
-      def from_store(endpoint_url,variable_in=nil, variable_out=nil, verbose=false)
-        raise "not implemented yet"
-      end
+      def from_store(repo,dataset=nil, verbose=false)
+        data = observation_hash(execute_from_file("observations.rq",repo,:graph,{"%{dataSet}"=>"<#{dataSet}>"}), true)
+        
+        # TODO - these need to be restricted to a single dataset as with the observations
+        dims = execute_from_file("dimensions.rq",repo,:graph).to_h.map{|d| [d[:dimension].to_s, d[:label].to_s]}
+        meas = execute_from_file("measures.rq",repo,:graph).to_h.map{|m| [m[:measure].to_s, m[:label].to_s]}
+        relation = execute_from_file("dataset.rq",repo,:graph).to_h.first[:label].to_s
+        codes = execute_from_file("codes.rq",repo,:graph).to_h.map{|e| e.values.map(&:to_s)}.inject({}){|h,el|
+          (h[el.first]||=[]) << el.last; h
+        }
+        attributes = {}
+        (dims | meas).map{|component|
+          attributes[component[1]] = case recommend_range(data.map{|o| o[1][component[1]]})
+            when "xsd:int"
+              "integer"
+            when "xsd:double"
+              "real"
+            when :coded
+              if dims.include? component
+                "{#{codes[component[1]].join(',')}}"
+              else
+                "string"
+              end
+            end
+        }
+        build_arff(dataset,attributes,data,dataset)
+       end
     end
   end
 end
