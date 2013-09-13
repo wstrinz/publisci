e5070db6a8ec2e5c039ca2d6083275cce570ca0a
Will Strinz <wstrinz@gmail.com>
2013-09-12 23:31:05 -0500

    basic repository adapter creation, views and start of import logic

--- server/helpers.rb
+++ server/helpers.rb
@@ -1,8 +1,38 @@
 class PubliSciServer < Sinatra::Base
 
   helpers do
-    def ba
-      settings.foo
+
+    def h(str)
+      CGI::escapeHTML(str.to_s)
+    end
+
+    def create_repository(type,uri)
+      if type == "in_memory"
+        flash[:notice] = "#{type} repository created!"
+        RDF::Repository.new
+      elsif type == "4store"
+        unless RDF::URI(uri).valid?
+          flash[:notice] = "Need a valid URI for a FourStore repository"
+          nil
+        end
+
+        flash[:notice] = "#{type} repository created!"
+        RDF::FourStore::Repository.new(uri)
+      else
+        raise "UnkownTypeForSomeReason?: #{type}"
+      end
+    end
+
+    def clear_repository
+      raise "not implemented yet"
+    end
+
+    def import_rdf(input,type)
+      if type == :file
+        raise "file type #{input.read}"
+      else
+        raise "string type #{type}"
+      end
     end
   end
 end
