a67e479b423445e556c47bed0051dc787bbfb145
Will Strinz <wstrinz@gmail.com>
2013-09-16 18:26:26 -0500

    full sadi query, with option to load to repository

--- scripts/get_gene_lengths.rb
+++ scripts/get_gene_lengths.rb
@@ -1,11 +1,7 @@
-require_relative '../lib/bio-publisci.rb'
+# require_relative '../lib/bio-publisci.rb'
+require 'bio-publisci'
 def gene_lengths(repo)
-  sadi_in = <<-EOF
-  @prefix sio: <http://semanticscience.org/resource/>.
-  @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.
-  @prefix serviceowl: <http://biordf.org:8080/sadi_service_ontologies/HUGO_2_ENSEMBLGENEINFO.owl/>.
 
-  EOF
   templ = <<-EOF
 
   EOF
@@ -20,20 +16,39 @@ def gene_lengths(repo)
   EOF
 
   genes = PubliSci::QueryHelper.execute(gene_query,repo).map(&:gene)
-  puts genes.size
-  genes[0..20].each{|gene|
-    sadi_in += <<-EOF
-    <#{gene}> a serviceowl:HUGO_2_ENSEMBLGENEINFO_Input;
-       sio:SIO_000300 "#{gene}".
+  puts "retrieving information for #{genes.size} genes"
+
+  all_genes = []
+  genes.each_slice(250){|slice|
+    sadi_in = <<-EOF
+    @prefix sio: <http://semanticscience.org/resource/>.
+    @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.
+    @prefix serviceowl: <http://biordf.org:8080/sadi_service_ontologies/HUGO_2_ENSEMBLGENEINFO.owl/>.
+
     EOF
+
+    slice.each{|gene|
+      sadi_in += <<-EOF
+      <#{gene}> a serviceowl:HUGO_2_ENSEMBLGENEINFO_Input;
+         sio:SIO_000300 "#{gene}".
+      EOF
+    }
+    response = PubliSci::SADI_request.fetch_async('http://biordf.org:8080/cgi-bin/services/HUGO_2_ENSEMBLGENEINFO_async.pl',sadi_in)
+    puts "chunk done #{ all_genes.size}"
+    all_genes << response.join("\n\n")
   }
-  open('sadi_in.ttl','w'){|f| f.write sadi_in}
-  # sadi_in = IO.read('sadi_in.ttl')
-  # response = SADI_request.send_request('http://biordf.org:8080/cgi-bin/services/HUGO_2_ENSEMBLGENEINFO_sync.pl',sadi_in)
-  response = PubliSci::SADI_request.fetch_async('http://biordf.org:8080/cgi-bin/services/HUGO_2_ENSEMBLGENEINFO_async.pl',sadi_in)
-  response.join("\n\n")
-  # open('gene_lengths.ttl','w'){|f| f.write response.join("\n\n")}
+  all_genes.join("\n\n")
 end
 
-# unless ARGV[0]
-  open('gene_lengths.ttl','w'){|f| f.write gene_lengths(RDF::FourStore::Repository.new('http://localhost:8080'))}
+repo_uri = ARGV[0] || 'http://localhost:8080'
+
+repo = RDF::FourStore::Repository.new(repo_uri)
+
+lengths = gene_lengths(repo)
+puts lengths
+if ARGV[1] == "save"
+  repo << RDF::Turtle::Reader.new(lengths)
+else
+  open('gene_lengths.ttl','w'){|f| f.write lengths }
+end
+
