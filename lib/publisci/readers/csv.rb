142234831b5ab8648f24c69bd3838fc2a51a7f75
Will Strinz <wstrinz@gmail.com>
2014-03-11 09:42:06 -0500

    fix csv reader indentation

--- lib/publisci/readers/csv.rb
+++ lib/publisci/readers/csv.rb
@@ -1,6 +1,6 @@
 module PubliSci
-	module Readers
-		class CSV
+  module Readers
+    class CSV
       include Base
       def automatic(file=nil,dataset_name=nil,options={},interactive=true)
         #to do
@@ -39,51 +39,51 @@ module PubliSci
         generate_n3(file,dataset_name,options)
       end
 
-			def generate_n3(file, dataset_name, options={})
-				@data = ::CSV.read(file)
-				@options = options
-				generate(measures, dimensions, codes, observation_data, observation_labels, dataset_name, options)
-			end
+      def generate_n3(file, dataset_name, options={})
+        @data = ::CSV.read(file)
+        @options = options
+        generate(measures, dimensions, codes, observation_data, observation_labels, dataset_name, options)
+      end
 
-			def dimensions
-				@options[:dimensions] || [@data[0][0]]
-			end
+      def dimensions
+        @options[:dimensions] || [@data[0][0]]
+      end
 
-			def codes
-				@options[:codes] || dimensions()
-			end
+      def codes
+        @options[:codes] || dimensions()
+      end
 
-			def measures
-				@options[:measures] || @data[0] - dimensions()
-			end
+      def measures
+        @options[:measures] || @data[0] - dimensions()
+      end
 
-			def observation_labels
-				if @options[:label_column]
-					tmp = @data.dup
-					tmp.shift
-					tmp.map{|row|
-						row[@options[:label_column]]
-					}
-				else
-					(1..@data.size - 1).to_a
-				end
-			end
+      def observation_labels
+        if @options[:label_column]
+          tmp = @data.dup
+          tmp.shift
+          tmp.map{|row|
+            row[@options[:label_column]]
+          }
+        else
+          (1..@data.size - 1).to_a
+        end
+      end
 
-			def observation_data
-				obs = {}
-				@data[0].map{|label|
-					obs[label] = []
-				}
-				tmp = @data.dup
-				tmp.shift
+      def observation_data
+        obs = {}
+        @data[0].map{|label|
+          obs[label] = []
+        }
+        tmp = @data.dup
+        tmp.shift
 
-				tmp.map{|row|
-					row.each_with_index{|entry,i|
-						obs[@data[0][i]] << entry
-					}
-				}
-				obs
-			end
-		end
-	end
+        tmp.map{|row|
+          row.each_with_index{|entry,i|
+            obs[@data[0][i]] << entry
+          }
+        }
+        obs
+      end
+    end
+  end
 end
